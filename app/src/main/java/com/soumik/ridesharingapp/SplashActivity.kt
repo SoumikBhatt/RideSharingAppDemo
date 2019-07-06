f822aacf52c45880e6cb45714ed589ce85420844
Soumik Bhattacharjee <soumikcse07@gmail.com>
2019-07-06 12:34:21 +0600

    Initial commit

---
+++ app/src/main/java/com/soumik/ridesharingapp/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.soumik.ridesharingapp
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
+
+class SplashActivity : AppCompatActivity() {
+
+    private val SPLASH_TIME_OUT : Long = 1500
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_splash)
+
+
+        Handler().postDelayed({
+            // This method will be executed once the timer is over
+            // Start your app main activity
+
+            startActivity(Intent(this, MainActivity::class.java))
+
+            // close this activity
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
