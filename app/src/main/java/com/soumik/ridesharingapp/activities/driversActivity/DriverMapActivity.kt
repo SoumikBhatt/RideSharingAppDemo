13d0935c614dc26e416cf54dec810ecc94316cd3
Soumik Bhattacharjee <soumikcse07@gmail.com>
2019-07-08 18:57:06 +0600

    driver location updated

--- app/src/main/java/com/soumik/ridesharingapp/activities/driversActivity/DriverMapActivity.kt
+++ app/src/main/java/com/soumik/ridesharingapp/activities/driversActivity/DriverMapActivity.kt
@@ -1,7 +1,13 @@
 package com.soumik.ridesharingapp.activities.driversActivity
 
+import android.annotation.SuppressLint
+import android.location.Location
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -11,9 +17,43 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.soumik.ridesharingapp.R
 
-class DriverMapActivity : AppCompatActivity(), OnMapReadyCallback {
+class DriverMapActivity : AppCompatActivity(), OnMapReadyCallback,
+                            GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+                            com.google.android.gms.location.LocationListener {
+
 
     private lateinit var mMap: GoogleMap
+    private lateinit var googleApiClient: GoogleApiClient
+    private lateinit var lastLocation: Location
+    private lateinit var locationRequest: LocationRequest
+
+    @SuppressLint("MissingPermission")
+    override fun onConnected(p0: Bundle?) {
+
+        locationRequest = LocationRequest()
+        locationRequest.interval=1000
+        locationRequest.fastestInterval=1000
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient,locationRequest,this)
+    }
+
+    override fun onConnectionSuspended(p0: Int) {
+
+    }
+
+    override fun onConnectionFailed(p0: ConnectionResult) {
+
+    }
+
+    override fun onLocationChanged(p0: Location?) {
+
+        lastLocation = p0!!
+
+        var latLng:LatLng = LatLng(p0.latitude,p0.longitude)
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng!!))
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(12F))
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,12 +73,28 @@ class DriverMapActivity : AppCompatActivity(), OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+    @SuppressLint("MissingPermission")
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        buildGoogleApiClient()
+
+        mMap.isMyLocationEnabled=true
+
+//        // Add a marker in Sydney and move the camera
+//        val sydney = LatLng(-34.0, 151.0)
+//        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    private fun buildGoogleApiClient(){
+
+        googleApiClient = GoogleApiClient.Builder(this)
+            .addConnectionCallbacks(this)
+            .addOnConnectionFailedListener(this)
+            .addApi(LocationServices.API)
+            .build()
+
+        googleApiClient.connect()
     }
 }
